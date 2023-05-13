edf4c6198858f4e41b424fb7981f83374d8f7158
Carlos Aliaga <carlosaligom@gmai.com>
2023-05-13 20:27:14 +0200

    Test two more cases of getRobots

--- src/server/controllers/robotsControllers.test.ts
+++ src/server/controllers/robotsControllers.test.ts
@@ -1,30 +1,62 @@
-import { type Request, type Response } from "express";
+import { type NextFunction, type Request, type Response } from "express";
 import { getRobots } from "./robotsControllers";
 import Robot from "../../database/models/Robot";
 import robotsListMock from "../../mocks/robotsMock";
+
+type CustomResponse = Pick<Response, "status" | "json">;
+
+const next = jest.fn();
 
 beforeEach(() => {
   jest.clearAllMocks();
 });
 
+const response: CustomResponse = {
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn(),
+};
 describe("Given a getRobots function controller", () => {
+  const request = {};
+
   describe("When it receives a response", () => {
+    Robot.find = jest.fn().mockReturnValue({
+      exec: jest.fn().mockResolvedValue(robotsListMock),
+    });
+
     test("Then it should call the response method status with 200", async () => {
       const expectedStatusCode = 200;
-      const next = jest.fn();
-
-      const request = {};
-      const response: Pick<Response, "status"> = {
-        status: jest.fn(),
-      };
-
-      Robot.find = jest.fn().mockReturnValue({
-        exec: jest.fn().mockResolvedValue(robotsListMock),
-      });
 
       await getRobots(request as Request, response as Response, next);
 
       expect(response.status).toHaveBeenCalledWith(expectedStatusCode);
+    });
+
+    test("Then it should call the response method json with a list of robots", async () => {
+      const expectedResponseBody = {
+        robots: robotsListMock,
+      };
+
+      await getRobots(request as Request, response as Response, next);
+
+      expect(response.json).toHaveBeenCalledWith(expectedResponseBody);
+    });
+  });
+
+  describe("When it receives a next a function and the exec method rejects with an 'Endpoint not found' error", () => {
+    test("Then it should call next function with error 'Endpoint not found'", async () => {
+      const error = new Error("Endpoint not found");
+
+      Robot.find = jest.fn().mockReturnValue({
+        exec: jest.fn().mockRejectedValue(error),
+      });
+
+      await getRobots(
+        request as Request,
+        response as Response,
+        next as NextFunction
+      );
+
+      expect(next).toHaveBeenCalledWith(error);
     });
   });
 });
