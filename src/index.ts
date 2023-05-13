f87a13597544a816ef70056a05c4c07b35bcae08
Carlos Aliaga <carlosaligom@gmai.com>
2023-05-13 12:53:57 +0200

    Refactor server config and port listener, and add mongoDb connection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-import express from "express";
+import "./loadEnviroments.js";
+import createDebug from "debug";
+import mongoose from "mongoose";
+import app from "./server/index.js";
 
-const app = express();
+const debug = createDebug("robots-api:root");
+const mongoDbConnection = process.env.MONGODB_CONNECTION;
 
-export default app;
+if (!mongoDbConnection) {
+  debug("connection error");
+  process.exit(1);
+}
+
+try {
+  await mongoose.connect(mongoDbConnection);
+  debug("Connection succesfull");
+} catch (error: unknown) {
+  debug(`Error ${(error as Error).message}`);
+}
+
+const port = process.env.PORT ?? 4000;
+
+app.listen(port, () => {
+  debug(`Listening on http://localhost:${port}`);
+});
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,15 +1,11 @@
 import "../loadEnviroments.js";
 import morgan from "morgan";
-import app from "../index.js";
-import debug from "debug";
 import express from "express";
 
-const port = process.env.PORT ?? 4000;
-
-app.use(express.json());
+const app = express();
 
 app.use(morgan("dev"));
 
-app.listen(port, () => {
-  debug(`Listening on http://localhost:${port}`);
-});
+app.use(express.json());
+
+export default app;
