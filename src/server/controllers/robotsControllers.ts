31ec48c34fc8a2aa0d6662da8a1d6e062872abb6
Frank Pereira <frankpereirag000@gmail.com>
2023-05-14 13:27:11 +0200

    Fix sonar coverage settings

--- src/server/controllers/robotsControllers.ts
+++ src/server/controllers/robotsControllers.ts
@@ -1,5 +1,6 @@
 import { type Response, type Request, type NextFunction } from "express";
 import Robot from "../../database/models/Robot.js";
+import CustomError from "../CustomError.js";
 
 export const getRobots = async (
   req: Request,
@@ -9,6 +10,30 @@ export const getRobots = async (
   try {
     const robots = await Robot.find().exec();
     res.status(200).json({ robots });
+  } catch (error: unknown) {
+    next(error);
+  }
+};
+
+export const deleteRobot = async (
+  req: Request<{ id: string }>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+
+  try {
+    const robot = await Robot.findByIdAndDelete(id).exec();
+
+    if (!robot) {
+      const error = new CustomError(404, "Robot not found");
+
+      throw error;
+    }
+
+    res
+      .status(200)
+      .json({ message: `Robot with the ${robot._id.toString()} id deleted ` });
   } catch (error: unknown) {
     next(error);
   }
